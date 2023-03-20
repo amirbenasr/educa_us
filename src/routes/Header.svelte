18224901dc298bcba7ce6f345a69bff2ee95978b
amir <amir@dragonchain.com>
2023-03-20 14:52:43 +0100

    first commit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,149 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+	import educa_logo from '$lib/images/educa_logo.png';
+</script>
+
+<header>
+	<div class="logos">
+		<div class="corner logo-span">
+			<a href="#">
+				<span class="logo-title"> <span class="capital-title">E</span> | Educa US</span>
+			</a>
+		</div>
+	</div>
+
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+				<a href="/sverdle">Sverdle</a>
+			</li>
+		</ul>
+		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg> -->
+	</nav>
+</header>
+
+<style>
+	.logos {
+		transition: all;
+		width: 50px;
+		transition-duration: 3s;
+	}
+	.logos:hover {
+		width: 500px;
+		background-color: red;
+		/* transform: translateX(200px); */
+	}
+
+	.logo-span span {
+		vertical-align: middle !important;
+	}
+	.capital-title {
+		font-weight: bold;
+		font-size: 72px;
+	}
+	.logo-title {
+		font-family: Georgia, 'Times New Roman', Times, serif;
+		font-size: 40px;
+		font-weight: bold;
+		color: white;
+		font-size: x-large;
+		margin-left: 4em;
+	}
+	header {
+		height: auto;
+		display: flex;
+		justify-content: space-between;
+		background-color: var(--color-theme-1);
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 3em;
+		height: 3em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>
