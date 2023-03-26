e8c48311df53fc470de5d05024b781fb4f360997
amir <amir@dragonchain.com>
2023-03-26 16:27:47 +0100

    change images to static

--- src/components/Testimonials.svelte
+++ src/components/Testimonials.svelte
@@ -1,13 +1,27 @@
 <script>
+	import { testimonials } from '../utils';
 </script>
 
 <section class="testimonials">
 	<div class="section-title">
 		<h1>Testimonials</h1>
 	</div>
+	<div class="section-body">
+		<div class="section-left">left</div>
+		<div class="section-right">right</div>
+	</div>
+	<div class="section-footer">circles</div>
 </section>
 
 <style>
+	.section-body {
+		display: flex;
+		justify-content: space-around;
+	}
+	.section-footer {
+		height: 80px;
+		background-color: red;
+	}
 	.testimonials {
 		height: 64rem;
 	}
--- src/utils.ts
+++ src/utils.ts
@@ -6,8 +6,38 @@ type Testimonial = {
 
 export const testimonials: Testimonial[] = [
 	{
-		image: '',
-		student_name: '',
+		image: 'img_1',
+		student_name: 'Aziz Bardi',
+		testimonial: ''
+	},
+
+	{
+		image: 'img_2',
+		student_name: 'Mayssa mehouechi',
+		testimonial: ''
+	},
+
+	{
+		image: 'img_3',
+		student_name: 'Rani  maghrbi',
+		testimonial: ''
+	},
+
+	{
+		image: 'img_4',
+		student_name: 'Safouene ben rjeb',
+		testimonial: ''
+	},
+
+	{
+		image: 'img_5',
+		student_name: 'Khalil chatti',
+		testimonial: ''
+	},
+
+	{
+		image: 'img_6',
+		student_name: 'Yessin dahmene',
 		testimonial: ''
 	}
 ];
